869ae9aeda48204909f0a4b6e560dfbec2a6b911
Gharbi <leithgharbi@yahoo.fr>
2022-10-27 13:58:05 +0100

    Other Uses of Binding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>Payment</h3>
+    <div v-if="error" class="alert alert-danger">{{ error }}</div>
     <form novalidate @submit.prevent="onSave">
       <div class="row">
         <div class="col-md-6">
@@ -12,7 +13,7 @@
               id="name"
               class="form-control"
               placeholder="Your Name"
-              v-model="payment.fullName"
+              v-model="payment.shipping.fullName"
             />
           </div>
           <div class="form-group">
@@ -22,7 +23,7 @@
               id="company"
               class="form-control"
               placeholder="Company"
-              v-model="payment.company"
+              v-model="payment.shipping.company"
             />
           </div>
           <div class="form-group">
@@ -32,7 +33,7 @@
               id="address1"
               class="form-control"
               placeholder="Street Address"
-              v-model="payment.address1"
+              v-model="payment.shipping.address1"
             />
           </div>
           <div class="form-group">
@@ -42,7 +43,7 @@
               id="address2"
               class="form-control"
               placeholder=""
-              v-model="payment.address2"
+              v-model="payment.shipping.address2"
             />
           </div>
           <div class="form-row">
@@ -53,7 +54,7 @@
                 id="cityTown"
                 class="form-control"
                 placeholder="e.g. New York"
-                v-model="payment.cityTown"
+                v-model="payment.shipping.cityTown"
               />
             </div>
             <div class="form-group col-md-3">
@@ -61,7 +62,7 @@
               <select
                 id="stateProvince"
                 class="form-control"
-                v-model="payment.stateProvince"
+                v-model="payment.shipping.stateProvince"
               >
                 <option
                   v-for="s in states"
@@ -79,7 +80,7 @@
                 id="postalCode"
                 class="form-control"
                 placeholder="e.g. 10101"
-                v-model="Zipcode"
+                v-model="payment.shipping.postalCode"
               />
             </div>
             <div class="form-group">
@@ -89,6 +90,83 @@
         </div>
         <div class="col-md-6">
           <div><strong>Billing Information</strong></div>
+          <div class="form-check">
+            <input
+              type="checkbox"
+              id="sameAsShipping"
+              class="form-check-input"
+              v-model="payment.billing.sameAsShipping"
+            />
+            <label for="sameAsShipping" class="form-check-label"
+              >Same As Shipping?</label
+            >
+          </div>
+          <div class="form-group">
+            <label for="address1">Address</label>
+            <input
+              type="text"
+              id="address1"
+              class="form-control"
+              placeholder="Street Address"
+              v-model="payment.billing.address1"
+              :disabled="payment.billing.sameAsShipping"
+            />
+          </div>
+          <div class="form-group">
+            <label for="address1">Suite/Appartement</label>
+            <input
+              type="text"
+              id="address2"
+              class="form-control"
+              placeholder=""
+              v-model="payment.billing.address2"
+              :disabled="payment.billing.sameAsShipping"
+            />
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="cityTown">cityTown</label>
+              <input
+                type="text"
+                id="cityTown"
+                class="form-control"
+                placeholder="e.g. New York"
+                v-model="payment.billing.cityTown"
+                :disabled="payment.billing.sameAsShipping"
+              />
+            </div>
+            <div class="form-group col-md-3">
+              <label for="stateProvince">State</label>
+              <select
+                id="stateProvince"
+                class="form-control"
+                v-model="payment.billing.stateProvince"
+                :disabled="payment.billing.sameAsShipping"
+              >
+                <option
+                  v-for="s in states"
+                  :key="s.abbreviation"
+                  :value="s.abbreviation"
+                >
+                  {{ stateFormat(s) }}
+                </option>
+              </select>
+            </div>
+            <div class="form-group col-md-3">
+              <label for="postalCode">Zipcode</label>
+              <input
+                type="text"
+                id="postalCode"
+                class="form-control"
+                placeholder="e.g. 10101"
+                v-model="payment.billing.postalCode"
+                :disabled="payment.billing.sameAsShipping"
+              />
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Next" class="btn btn-success" />
+            </div>
+          </div>
         </div>
       </div>
     </form>
@@ -98,18 +176,26 @@
   </div>
 </template>
 <script>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import states from "@/lookup/states";
 import formatters from "@/formatters";
 export default {
   // this is for composition api
   setup() {
     const payment = ref({
-      fullName: "Shawn",
-      postalCode: "12345",
+      shipping: {
+        fullName: "Shawn",
+        postalCode: "12345",
+      },
+      billing: {
+        sameAsShipping: false,
+      },
     });
+
+    const error = ref("");
+
     function onSave() {
-      alert("save");
+      error.value = "We can't save yet, we don't have an API";
     }
 
     const Zipcode = computed({
@@ -127,8 +213,24 @@ export default {
       },
     });
 
+    watch(
+      // what to watch
+      () => payment.value.billing.sameAsShipping,
+      // What to do
+      () => {
+        if (payment.value.billing.sameAsShipping) {
+          payment.value.billing.address1 = "";
+          payment.value.billing.address2 = "";
+          payment.value.billing.cityTown = "";
+          payment.value.billing.stateProvince = "";
+          payment.value.billing.postalCode = "";
+        }
+      }
+    );
+
     return {
       payment,
+      error,
       states,
       onSave,
       ...formatters,
